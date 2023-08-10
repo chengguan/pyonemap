3aefd8e40abf568c151fe4092b8baff6d2967e75
Teo Cheng Guan <cheng_guan_teo@msi-global.com.sg>
2023-08-10 11:44:41 +0800

    Add routing and coordinate converters

--- pyonemap/core.py
+++ pyonemap/core.py
@@ -5,14 +5,18 @@ __version__ = "1.0"
 
 import requests
 import os
+from datetime import datetime
 
 class OneMap:
     def __init__(self, api_key=None):
-        self.base_url = "https://www.onemap.gov.sg"
-        self.api_key = api_key
+        self.__base_url = "https://www.onemap.gov.sg"
+        self.__api_key = api_key
 
-    def getToken(self, email, password):
-        url = f"{self.base_url}/api/auth/post/getToken"
+    def setToken(self, access_token):
+        self.__api_key = access_token
+
+    def getToken(email, password):
+        url = "https://www.onemap.gov.sg/api/auth/post/getToken"
         payload = {"email": email, "password": password}
 
         try:
@@ -26,11 +30,11 @@ class OneMap:
         return response.json()
 
     def _make_request(self, endpoint, params):
-        url = f"{self.base_url}{endpoint}"
+        url = f"{self.__base_url}{endpoint}"
         headers = {}
 
-        if self.api_key:
-            headers["Authorization"] = f"Bearer {self.api_key}"
+        if self.__api_key:
+            headers["Authorization"] = f"Bearer {self.__api_key}"
 
         response = requests.get(url, params=params, headers=headers)
 
@@ -113,9 +117,159 @@ class OneMap:
         
         return response
 
+    '''
+        start string REQUIRED
+        The start point in WGS84 latitude, longitude format.
+
+        end string REQUIRED
+        The end point in WGS84 latitude, longitude format.
+
+        routeType string REQUIRED
+        Route types available pt (default), walk, drive and cycle. Only lowercase is allowed.
+
+        date string REQUIRED
+        Date of the selected start point in MM-DD-YYYY.
+
+        time string REQUIRED
+        Time of the selected start point in HH:MM:SS, using the 24-hour clock system. 
+        [HH] refers to a zero-padded hour between 00 and 23. 
+        [MM] refers to a zero-padded minute between 00 and 59. 
+        [SS] refers to a zero-padded second between 00 and 59.
+
+        mode string REQUIRED
+        Mode of public transport: TRANSIT (default), BUS, RAIL. Entry must be specified in UPPERCASE
+
+        maxWalkDistance string Optional. 
+        The maximum walking distance set by the user in metres. Default is 1000m.
+
+        numItineraries string Optional. 
+        Limits the number of return results: 1 to 3.
+    '''
+    def route(self, start_lat, start_lon, end_lat, end_lon, 
+                    routeType='pt', date='', time='', mode='TRANSIT', 
+                    maxWalkDistance=1000, numItineraries=3):
+        endpoint = f"/api/public/routingsvc/route"
+        available_modes = ['TRANSIT', 'BUS', 'RAIL']
+        
+        if routeType == 'walk' or routeType == 'drive' or routeType == 'cycle':
+            params = {"start": f'{start_lat},{start_lon}', 
+                      "end": f'{end_lat},{end_lon}', 
+                      "routeType": routeType}
+        elif routeType == 'pt':
+            if mode not in available_modes:
+                print(f'Error: invalid mode of transport: {mode}.')
+                return None
+
+            # Fill up with the current date and time if the inputs are not given.
+            now = datetime.now()
+            if date == '':
+                date = now.strftime('%m-%d-%Y')
+            if time == '':
+                time = now.strftime('%H:%M:%S')
+
+            params = {"start": f'{start_lat},{start_lon}', 
+                      "end": f'{end_lat},{end_lon}', 
+                      "routeType": routeType, 
+                      "date": date, 
+                      "time": time,
+                      "mode": mode,
+                      "maxWalkDistance": maxWalkDistance,
+                      "numItineraries": numItineraries}
+        else:
+            print(f'Error: invalid routeType: {routeType}.')
+            return None
+
+        response = self._make_request(endpoint, params)
+        
+        return response
+
+    '''
+        latitude string REQUIRED
+        Latitude coordinates in WGS84 format.
+
+        longitude string REQUIRED
+        Longitude coordinates in WGS84 format.
+    '''
+    def convert4326to3857(self, lat, lon):
+        endpoint = f"/api/common/convert/4326to3857"
+        params = {"latitude": lat, "longitude": lon}
+        response = self._make_request(endpoint, params)
+        return response
+
+    '''
+        latitude string REQUIRED
+        Latitude coordinates in WGS84 format.
+
+        longitude string REQUIRED
+        Longitude coordinates in WGS84 format.
+    '''
+    def convert4326to3414(self, lat, lon):
+        endpoint = f"/api/common/convert/4326to3414"
+        params = {"latitude": lat, "longitude": lon}
+        response = self._make_request(endpoint, params)
+        return response
+
+    '''
+        Y string REQUIRED
+        Y coordinates in SVY21 format.
+
+        X string REQUIRED
+        X coordinates in SVY21 format.
+    '''
+    def convert3414to3857(self, x, y):
+        endpoint = f"/api/common/convert/3414to3857"
+        params = {"X": x, "Y": y}
+        response = self._make_request(endpoint, params)
+        return response
+
+    '''
+        Y string REQUIRED
+        Y coordinates in SVY21 format.
+
+        X string REQUIRED
+        X coordinates in SVY21 format.
+    '''
+    def convert3414to4326(self, x, y):
+        endpoint = f"/api/common/convert/3414to4326"
+        params = {"X": x, "Y": y}
+        response = self._make_request(endpoint, params)
+        return response
+
+    '''
+        Y string REQUIRED
+        Y coordinates in SVY21 format.
+
+        X string REQUIRED
+        X coordinates in SVY21 format.
+    '''
+    def convert3857to3414(self, y, x):
+        endpoint = f"/api/common/convert/3857to3414"
+        params = {"Y": y, "X": x}
+        response = self._make_request(endpoint, params)
+        return response
+
+    '''
+        Y string REQUIRED
+        Y coordinates in SVY21 format.
+
+        X string REQUIRED
+        X coordinates in SVY21 format.
+    '''
+    def convert3857to4326(self, y, x):
+        endpoint = f"/api/common/convert/3857to4326"
+        params = {"Y": y, "X": x}
+        response = self._make_request(endpoint, params)
+        return response
 
 if __name__ == "__main__":
-    onemap = OneMap()
+    # Obtain the access token using email and password:
+    response = OneMap.getToken('your_email@email.com', 'your_password')
+    access_token = response['access_token']
+    
+    # Or, pre-set it as environmental variable:
+    #access_token = os.environ.get("ONEMAP_TOKEN")
+
+    onemap = OneMap(access_token)
     
     location = onemap.search("5 Havelock Road", 'Y', 'Y')
     print(location)
@@ -125,3 +279,24 @@ if __name__ == "__main__":
 
     geocode = onemap.revGeoCode(1.3254295, 103.9005321)
     print(geocode)
+
+    route = onemap.route(1.320981, 103.844150, 1.326762, 103.8559)
+    print(route)
+
+    coordinate = onemap.convert4326to3857(1.319728905, 103.8421581)
+    print(coordinate)
+    
+    coordinate = onemap.convert4326to3414(1.319728905, 103.8421581)
+    print(coordinate)
+
+    coordinate = onemap.convert3414to3857(28983.788791079794, 33554.509813284)
+    print(coordinate)
+
+    coordinate = onemap.convert3414to4326(28983.788791079794, 33554.5098132845)
+    print(coordinate)
+
+    coordinate = onemap.convert3857to3414(146924.54200324757, 11559656.16256661)
+    print(coordinate)
+
+    coordinate = onemap.convert3857to4326(146924.54200324757, 11559656.16256661)
+    print(coordinate)
