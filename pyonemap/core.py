c02cc8ecd9db35274e2256a28a620f1f80246d4c
Teo Cheng Guan <cheng_guan_teo@msi-global.com.sg>
2023-08-10 12:04:23 +0800

    add unit test result in comments

--- pyonemap/core.py
+++ pyonemap/core.py
@@ -65,7 +65,7 @@ class OneMap:
                   "pageNum": pageNum}
 
         response = self._make_request(endpoint, params)
-        return response.get("results", [])
+        return response
 
     '''
         location string
@@ -271,32 +271,60 @@ if __name__ == "__main__":
 
     onemap = OneMap(access_token)
     
-    location = onemap.search("5 Havelock Road", 'Y', 'Y')
+    location = onemap.search("200640", 'Y', 'Y')
     print(location)
+    # {'found': 1, 'totalNumPages': 1, 'pageNum': 1, 
+    # 'results': [{'SEARCHVAL': '640 ROWELL ROAD SINGAPORE 200640', 
+    #   'BLK_NO': '640', 'ROAD_NAME': 'ROWELL ROAD', 'BUILDING': 'NIL', 
+    #   'ADDRESS': '640 ROWELL ROAD SINGAPORE 200640', 'POSTAL': '200640', 
+    #   'X': '30381.1007417506', 'Y': '32195.1006872542', 
+    #   'LATITUDE': '1.30743547948389', 'LONGITUDE': '103.854713903431'}]}
 
     geocode = onemap.revGeoCodeXy(24291.97788882387, 31373.0117224489)
     print(geocode)
+    # {'GeocodeInfo': [{'BUILDINGNAME': 'NEW TOWN PRIMARY SCHOOL', 
+    #       'BLOCK': '300', 'ROAD': 'TANGLIN HALT ROAD', 'POSTALCODE': '148812', 
+    #       'XCOORD': '24303.3101027', 'YCOORD': '31333.3389857', 
+    #       'LATITUDE': '1.2996418818103135', 'LONGITUDE': '103.80010216304007'}]}
 
     geocode = onemap.revGeoCode(1.3254295, 103.9005321)
     print(geocode)
+    # {'GeocodeInfo': [{'BUILDINGNAME': 'KAMPONG UBI VIEW', 
+    #       'BLOCK': '351', 'ROAD': 'UBI AVENUE 1', 'POSTALCODE': '400351', 
+    #       'XCOORD': '35501.9607216', 'YCOORD': '34191.1578935', 
+    #       'LATITUDE': '1.325486284730739', 'LONGITUDE': '103.90072773995409'}, 
+    #       {'BUILDINGNAME': 'KAMPONG UBI VIEW', 
+    #       'BLOCK': '352', 'ROAD': 'UBI AVENUE 1', 'POSTALCODE': '400352', 
+    #       'XCOORD': '35354.2225417', 'YCOORD': '34162.4855169', 
+    #       'LATITUDE': '1.3252270180708603', 'LONGITUDE': '103.89940022649942'}, 
+    #       {'BUILDINGNAME': 'KAMPONG UBI VIEW', 
+    #       'BLOCK': '349', 'ROAD': 'UBI AVENUE 1', 'POSTALCODE': '400349', 
+    #       'XCOORD': '35485.6405577', 'YCOORD': '34228.9805541', 
+    #       'LATITUDE': '1.3258283432645641', 'LONGITUDE': '103.90058110378057'}]}
 
     route = onemap.route(1.320981, 103.844150, 1.326762, 103.8559)
     print(route)
 
     coordinate = onemap.convert4326to3857(1.319728905, 103.8421581)
     print(coordinate)
+    # {'Y': 146924.54200324757, 'X': 11559656.16256661}
     
     coordinate = onemap.convert4326to3414(1.319728905, 103.8421581)
     print(coordinate)
+    # {'Y': 33554.4360965479, 'X': 28983.75169436287}
 
     coordinate = onemap.convert3414to3857(28983.788791079794, 33554.509813284)
     print(coordinate)
+    # {'Y': 146924.61623595929, 'X': 11559656.199673364}
 
     coordinate = onemap.convert3414to4326(28983.788791079794, 33554.5098132845)
     print(coordinate)
+    # {'latitude': 1.3197295716669164, 'longitude': 103.84215843333567}
 
     coordinate = onemap.convert3857to3414(146924.54200324757, 11559656.16256661)
     print(coordinate)
+    # {'Y': 33554.4360965483, 'X': 28983.75169436287}
 
     coordinate = onemap.convert3857to4326(146924.54200324757, 11559656.16256661)
     print(coordinate)
+    # {'latitude': 1.3197289050000036, 'longitude': 103.8421581}
